f9ae90a31aeb7faa6bdf537d02e95cf1d80206da
Bruno Ferreira da Silva <92815660+brunoF-Silva@users.noreply.github.com>
2022-08-18 16:35:33 -0300

    Both ways of using it
    linking the js with react to the div

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-ReactDOM.render(<h1>Hello Bruno!</h1>, document.querySelector("#root"))
+// ReactDOM.render(<h1>Hello Bruno!</h1>, document.querySelector("#root"))
+
+ReactDOM.render(<p>bla bla bla</p>, document.getElementById("root"))
